012a5a732420191d61935862a35d4b00ee1f7096
AlvaroGarciaMartin <gam.roalva@hotmail.com>
2024-11-19 18:58:27 +0100

    crear atributos clase Alumno

--- src/main/java/org/iesalandalus/programacion/matriculacion/dominio/Alumno.java
+++ src/main/java/org/iesalandalus/programacion/matriculacion/dominio/Alumno.java
@@ -1,5 +1,23 @@
 package org.iesalandalus.programacion.matriculacion.dominio;
 
+import java.time.LocalDate;
+
 public class Alumno {
-    
+
+  private String ER_TELEFONO;
+  private  String ER_CORREO;
+  private String ER_DNI;
+  public String FORMATO_FECHA;
+  private String ER_NIA;
+  private int MIN_EDAD_ALUMNO;
+  private String nombre;
+  private String telefono;
+  private String correo;
+  private String dni;
+  private LocalDate fechaNacimiento;
+  private String nia;
+
+  public Alumno(){
+
+  }
 }
