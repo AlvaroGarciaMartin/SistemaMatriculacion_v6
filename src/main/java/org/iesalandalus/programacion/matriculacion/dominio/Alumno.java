a8e0e7b6bb3f16d914fd26e2351a28e76ba8100c
AlvaroGarciaMartin <gam.roalva@hotmail.com>
2024-11-23 11:03:39 +0100

    el alumno no puede tener menos de 16 años

--- src/main/java/org/iesalandalus/programacion/matriculacion/dominio/Alumno.java
+++ src/main/java/org/iesalandalus/programacion/matriculacion/dominio/Alumno.java
@@ -1,6 +1,7 @@
 package org.iesalandalus.programacion.matriculacion.dominio;
 
 import java.time.LocalDate;
+import java.util.Date;
 import java.util.regex.*;
 
 public class Alumno {
@@ -10,13 +11,14 @@ public class Alumno {
   private String ER_DNI;
   public String FORMATO_FECHA;
   private String ER_NIA;
-  private int MIN_EDAD_ALUMNO;
+  private int MIN_EDAD_ALUMNADO = 16;
   private String nombre;
   private String telefono;
   private String correo;
   private String dni;
   private LocalDate fechaNacimiento;
   private String nia;
+
 
   public Alumno(String nombre,String telefono,String correo,String dni,LocalDate fechaNacimiento,String nia){
     setNombre(nombre);
@@ -177,7 +179,9 @@ public class Alumno {
   }
 
   public void setFechaNacimiento(LocalDate fechaNacimiento) {
-
+    if (fechaNacimiento.getYear() - LocalDate.now().getYear() < MIN_EDAD_ALUMNADO) {
+      throw new IllegalArgumentException("El Alumno no puede tener menos de 16 años");
+    }
     this.fechaNacimiento = fechaNacimiento;
   }
 
