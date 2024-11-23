1d8cc024dbd6afb221ec83339d595ed0f6fabab3
AlvaroGarciaMartin <gam.roalva@hotmail.com>
2024-11-23 11:45:09 +0100

    crear curso

---
+++ src/main/java/org/iesalandalus/programacion/matriculacion/dominio/Curso.java
@@ -0,0 +1,21 @@
+package org.iesalandalus.programacion.matriculacion.dominio;
+
+public enum Curso {
+    PRIMERO("Primero"),SEGUNDO("Segundo");
+    private String cadenaAMostrar;
+    private Color(String cadenaAMostrar)
+    {
+        this.cadenaAMostrar=cadenaAMostrar;
+    }
+    //Crear cadenaAMostrar//
+    @Override
+    public String toString() {
+        int digito=0;
+        if (cadenaAMostrar == PRIMERO.cadenaAMostrar) {
+         digito=0;
+        } else {
+            digito=1;
+        }
+        return digito+".-"+cadenaAMostrar;
+    }
+}
