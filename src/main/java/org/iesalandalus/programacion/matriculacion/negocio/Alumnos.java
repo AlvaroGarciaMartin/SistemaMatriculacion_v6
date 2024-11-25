682bb368d993579c9437735202bfb4a560b491b9
AlvaroGarciaMartin <gam.roalva@hotmail.com>
2024-11-25 18:14:39 +0100

    corregir constructor alumno

--- src/main/java/org/iesalandalus/programacion/matriculacion/negocio/Alumnos.java
+++ src/main/java/org/iesalandalus/programacion/matriculacion/negocio/Alumnos.java
@@ -2,4 +2,30 @@ package org.iesalandalus.programacion.matriculacion.negocio;
 
 public class Alumnos {
 
+   private int capacidad;
+   private int tamano=0;
+
+   public Alumnos (int capacidad) {
+       setCapacidad(capacidad);
+       setTamaño(tamano);
+   }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+
+       this.capacidad = capacidad;
+    }
+
+    public int getTamaño() {
+
+       return tamano;
+    }
+
+    public void setTamaño(int tamano) {
+        this.tamano = tamano;
+    }
+
 }
