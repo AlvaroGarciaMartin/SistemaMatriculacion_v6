a3e1e9ebc53f6885f103707dcb1cf91716968a37
ALVARO GARCIA MARTIN <gam.roalva@hotmail.com>
2025-03-14 11:12:30 +0100

    crear FuenteDatosMySQL

---
+++ src/main/java/org/iesalandalus/programacion/matriculacion/modelo/negocio/mysql/FuenteDatosMySQL.java
@@ -0,0 +1,25 @@
+package org.iesalandalus.programacion.matriculacion.modelo.negocio.mysql;
+
+import org.iesalandalus.programacion.matriculacion.modelo.negocio.*;
+
+public class FuenteDatosMySQL implements IFuenteDatos {
+    @Override
+    public IAlumnos crearAlumnos() {
+        return new Alumnos();
+    }
+
+    @Override
+    public IAsignaturas crearAsignaturas() {
+        return new Asignaturas();
+    }
+
+    @Override
+    public ICiclosFormativos crearCiclosFormativos() {
+        return new CiclosFormativos();
+    }
+
+    @Override
+    public IMatriculas crearMatriculas() {
+        return new Matriculas();
+    }
+}
