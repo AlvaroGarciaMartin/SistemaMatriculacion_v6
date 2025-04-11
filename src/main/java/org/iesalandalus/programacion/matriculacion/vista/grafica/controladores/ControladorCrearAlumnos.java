e3a67a9ee9a44a52662f0b183c904a05990f84d2
ALVARO GARCIA MARTIN <gam.roalva@hotmail.com>
2025-04-11 18:26:41 +0200

    correccion de consultas

--- src/main/java/org/iesalandalus/programacion/matriculacion/vista/grafica/controladores/ControladorCrearAlumnos.java
+++ src/main/java/org/iesalandalus/programacion/matriculacion/vista/grafica/controladores/ControladorCrearAlumnos.java
@@ -1,4 +1,43 @@
 package org.iesalandalus.programacion.matriculacion.vista.grafica.controladores;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 
 public class ControladorCrearAlumnos {
+
+    @FXML
+    private Button btnAceptarAlumno;
+
+    @FXML
+    private DatePicker dpFechaNac;
+
+    @FXML
+    private HBox hbCorreoAlumno;
+
+    @FXML
+    private HBox hbDniAlumno;
+
+    @FXML
+    private HBox hbFechaNac;
+
+    @FXML
+    private HBox hbNombreAlumno;
+
+    @FXML
+    private HBox hbTelefonoAlumno;
+
+    @FXML
+    private TextField tfCorreoAlumno;
+
+    @FXML
+    private TextField tfDniAlumno;
+
+    @FXML
+    private TextField tfNombreAlumno;
+
+    @FXML
+    private TextField tfTelefonoAlumno;
+
 }
